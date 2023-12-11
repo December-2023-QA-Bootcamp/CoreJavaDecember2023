8f0637ecbb6ed50d2f9ede76ddc9fa7bb93414ef
TofaelKabir <tofaelkabir@gmail.com>
2023-12-10 21:13:47 -0500

    Java Basic 01

---
+++ src/lec01_java_basic/NamingConvention.java
@@ -0,0 +1,30 @@
+package lec01_java_basic;
+
+// Project names generally starts with UpperCase --> December2023CoreJava
+// Package names generally starts with lowerCase --> lec01JavaBasic
+// Class names starts with UpperCase  --> NamingConvention 
+// Class names are usually nouns or noun phrases
+
+// Project, package, Class follow Camel case or Snake case pattern. [start from second word]
+
+// Camel case feature considered from second word (not from the first word or second letter)
+// There is No space and second word start with upper case, same for third, fourth, fifth words]
+// Camel case example for a Project --> December2023CoreJava
+// Camel case example for a package --> lec01JavaBasic
+// Camel case example for a Class --> NamingConvention
+
+// Alternative of Camel case (Called snake_case). 
+// words separated by under score and from second word always lower case
+// Snake case feature considered from second word, (not from the first word)
+// Snake case example for a Project --> December_2023_core_java
+// Snake case example for a package --> lec01_java_basic
+// Snake case example for a Class --> Naming_convention
+
+public class NamingConvention {
+
+	public static void main(String[] args) {
+		System.out.println("This is all about Naming Convention");
+
+	}
+
+}
